a74566a61e7387895aa47156da00056590cf3442
aitemr <aitemr16@gmail.com>
2026-05-26 12:04:30 +0500

    Initial Commit

---
+++ pdd/pddApp.swift
@@ -0,0 +1,17 @@
+//
+//  pddApp.swift
+//  pdd
+//
+//  Created by aitemr on 26.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct pddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
